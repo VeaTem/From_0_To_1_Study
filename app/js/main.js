fc4b3414587218494a15fa42bb18ff13c2dc4761
VeaTem <veatem.web@gmail.com>
2021-11-20 01:58:15 +0200

    Added Design, crate double filter

--- app/js/main.js
+++ app/js/main.js
@@ -5,5 +5,15 @@
      })
 
 
-     var mixer = mixitup('.products__gallery');
+
+
+     var containerEl1 = document.querySelector('[data-ref="container-1"]');
+     var containerEl2 = document.querySelector('[data-ref="container-2"]')
+     var config = {
+         controls: {
+             scope: 'local'
+         }
+     };
+     var mixer = mixitup(containerEl1, config);
+     var mixer = mixitup(containerEl2, config);
  });
