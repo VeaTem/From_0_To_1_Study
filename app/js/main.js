10aa840964364f8c932298382c5f44b7bb774509
VeaTem <veatem.web@gmail.com>
2021-11-26 22:12:38 +0200

    added price filter

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,28 @@
  $(function() {
+     $('.price-filter__input').ionRangeSlider({
+         onStart: function(data) {
+             $('.price-filter__from').text(data.from)
+             $('.price-filter__to').text(data.to)
+         },
+         onChange: function(data) {
+             $('.price-filter__from').text(data.from)
+             $('.price-filter__to').text(data.to)
+         },
+
+     });
+
+
+
      $('.slider__inner').slick({
          arrows: false,
          dots: true,
-     })
+     });
 
 
 
 
      var containerEl1 = document.querySelector('[data-ref="container-1"]');
-     var containerEl2 = document.querySelector('[data-ref="container-2"]')
+     var containerEl2 = document.querySelector('[data-ref="container-2"]');
      var config = {
          controls: {
              scope: 'local'
--- gulpfile.js
+++ gulpfile.js
@@ -34,6 +34,7 @@ function styles() {
 function scripts() {
     return src([
             'node_modules/jquery/dist/jquery.js',
+            'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
             'node_modules/slick-carousel/slick/slick.js',
             'node_modules/mixitup/dist/mixitup.min.js',
             'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
